e3aea339ad72a24a13ab90fddb22f98ed740380f
ncoop57 <nathanacoop@gmail.com>
2024-07-16 09:58:43 -0500

    chore: Refactor Game of Life implementation in fasthtml to use new ws api

--- game_of_life/main.py
+++ game_of_life/main.py
@@ -1,8 +1,6 @@
 from fasthtml.common import *
-from fastcore.xml import to_xml
-from starlette.endpoints import WebSocketEndpoint
-from starlette.routing import WebSocketRoute
 import asyncio
+import sys
 
 if __name__ == "__main__": sys.exit("Run this app with `uvicorn main:app`")
 
@@ -12,8 +10,7 @@ htmx_ws = Script(src="https://unpkg.com/htmx-ext-ws@2.0.0/ws.js")
 app = FastHTML(hdrs=(picolink, gridlink, css, htmx_ws))
 rt = app.route
 
-grid = [[0 for _ in range(20)] for _ in range(20)]
-game_state = {'running': False}
+game_state = {'running': False, 'grid': [[0 for _ in range(20)] for _ in range(20)]}
 def update_grid(grid: list[list[int]]) -> list[list[int]]:
     new_grid = [[0 for _ in range(20)] for _ in range(20)]
     def count_neighbors(x, y):
@@ -29,13 +26,12 @@ def update_grid(grid: list[list[int]]) -> list[list[int]]:
             if grid[i][j] == 1:
                 if neighbors < 2 or neighbors > 3: new_grid[i][j] = 0
                 else: new_grid[i][j] = 1
-            else:
-                if neighbors == 3: new_grid[i][j] = 1
+            elif neighbors == 3: new_grid[i][j] = 1
     return new_grid
 
 def Grid():
     cells = []
-    for y, row in enumerate(grid):
+    for y, row in enumerate(game_state['grid']):
         for x, cell in enumerate(row):
             cell_class = 'alive' if cell else 'dead'
             cell = Div(cls=f'cell {cell_class}', hx_put='/update', hx_vals={'x': x, 'y': y}, hx_swap='none', hx_target='#gol', hx_trigger='click')
@@ -47,55 +43,48 @@ def Home():
     run_btn = Button('Run', id='run', cls='col-xs-2', hx_put='/run', hx_target='#gol', hx_swap='none')
     pause_btn = Button('Pause', id='pause', cls='col-xs-2', hx_put='/pause', hx_target='#gol', hx_swap='none')
     reset_btn = Button('Reset', id='reset', cls='col-xs-2', hx_put='/reset', hx_target='#gol', hx_swap='none')
-    return (Title('Game of Life'), Main(
-            gol, Div(run_btn, pause_btn, reset_btn, cls='row center-xs'),
-            hx_ext="ws", ws_connect="/gol"))
+    return Title('Game of Life'), Main(gol, Div(run_btn, pause_btn, reset_btn, cls='row center-xs'), hx_ext="ws", ws_connect="/gol")
 
 @rt('/')
 def get(): return Home()
 
 player_queue = []
+async def update_players():
+    for i, player in enumerate(player_queue):
+        try: await player(Grid())
+        except: player_queue.pop(i)
 async def on_connect(send): player_queue.append(send)
-async def on_disconnect(send): player_queue.remove(send)
+async def on_disconnect(send): await update_players()
 
 @app.ws('/gol', conn=on_connect, disconn=on_disconnect)
 async def ws(msg:str, send): pass
 
 async def background_task():
-    global grid
     while True:
         if game_state['running'] and len(player_queue) > 0:
-            grid = update_grid(grid)
-            for player in player_queue: await player(Grid())
+            game_state['grid'] = update_grid(game_state['grid'])
+            await update_players()
         await asyncio.sleep(1.0)
 
 background_task_coroutine = asyncio.create_task(background_task())
 
 @rt('/update')
 async def put(x: int, y: int):
-    grid[y][x] = 1 if grid[y][x] == 0 else 0
-    for player in player_queue: await player(Grid())
+    game_state['grid'][y][x] = 1 if game_state['grid'][y][x] == 0 else 0
+    await update_players()
 
 @rt('/run')
 async def put():
     game_state['running'] = True
-    for player in player_queue: await player(Grid())
+    await update_players()
 
 @rt("/reset")
 async def put():
-    global grid
-    grid = [[0 for _ in range(20)] for _ in range(20)]
+    game_state['grid'] = [[0 for _ in range(20)] for _ in range(20)]
     game_state['running'] = False
-    for player in player_queue: await player(Grid())
+    await update_players()
 
 @rt('/pause')
 async def put():
     game_state['running'] = False
-    for player in player_queue: await player(Grid())
-
-# run_uv()
-# import uvicorn
-
-# if __name__ == "__main__":
-#     asyncio.run(background_task())
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    await update_players()
