8405cbddf188c26282d9411005aa915d0f9dc9c2
Jeremy Howard <github@jhoward.fastmail.fm>
2024-07-21 15:59:58 +1000

    helloworld

---
+++ helloworld/main.py
@@ -0,0 +1,13 @@
+from fasthtml.common import *
+from functools import cache
+
+app,rt = fast_app()
+
+@cache
+def home(): return Titled('Hello world', P('Greetings from FastHTML'))
+
+@rt("/")
+def get(): return home()
+
+run_uv()
+
