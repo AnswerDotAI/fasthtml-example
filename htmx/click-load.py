35d8fc268d6bb6b8d6d22d6af0b48c656190aa26
Jeremy Howard <github@jhoward.fastmail.fm>
2024-08-29 13:02:41 +1000

    async chat

--- htmx/click-load.py
+++ htmx/click-load.py
@@ -1,4 +1,5 @@
 from fasthtml.common import *
+from asyncio import sleep
 from random import randint
 import secrets
 
@@ -8,21 +9,24 @@ def mk_row():
     idx = randint(0,1000)
     return Tr( Td(f'Agent {idx}'), Td(f'void{idx}@null.org'), Td(secrets.token_hex(8)))
 
+def set_busy(b): return f"event.detail.elt.setAttribute('aria-busy', '{b}' )"
+
 @rt('/')
 def get():
-    return (
+    return Titled('Click load demo',
+        PicoBusy(),
+        P(I('Loading is delayed by 1 second to demonstrate use of "aria-busy" loading spinner in Pico.')),
         Table(
             Thead( Tr( Th('Name'), Th('Email'), Th('ID'))),
-            Tbody(
-                *[mk_row() for _ in range(5)],
-            id='agents')),
-        Button(
-                'Load Another Agent...',
-                hx_get='/more', hx_target='#agents', hx_swap='beforeend',
-                cls='btn primary')
+            Tbody( *[mk_row() for _ in range(5)], id='agents')),
+        Button('Load Another Agent...',
+               hx_get='/more', hx_target='#agents', hx_swap='beforeend', cls='btn primary')
     )
 
 @rt('/more')
-def get(): return mk_row()
+async def get():
+    await sleep(1)
+    return mk_row()
 
 serve()
+
