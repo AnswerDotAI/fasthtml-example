1caf58af8cb883ab07b23e124e8022fa432de97d
Pierre Carru <pierre@carru.fr>
2024-09-18 20:09:56 +0200

    fix oauth_example/oa.py

--- oauth_example/oa.py
+++ oauth_example/oa.py
@@ -1,7 +1,9 @@
 from fasthtml.common import *
 from fasthtml.oauth import GoogleAppClient, OAuth
 
-cli = GoogleAppClient.from_file('/Users/jhoward/git/nbs/oauth-test/client_secret.json')
+redirect_uri = "http://localhost:5001/redirect"
+cli = GoogleAppClient.from_file('/Users/jhoward/git/nbs/oauth-test/client_secret.json',
+                                redirect_uri=redirect_uri)
 
 class Auth(OAuth):
     def login(self, info, state): return RedirectResponse('/', status_code=303)
@@ -16,7 +18,9 @@ oauth = Auth(app, cli)
 def home(auth): return P('Logged in!'), A('Log out', href='/logout')
 
 @app.get('/login')
-def login(req): return Div(P("Not logged in"), A('Log in', href=oauth.login_link(req)))
+def login(req):
+    return Div(P("Not logged in"), A('Log in', href=cli.login_link(redirect_uri=redirect_uri)))
+
 
 serve()
 
