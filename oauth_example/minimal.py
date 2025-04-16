b3f7c36613e15fcfec49ad0de2ed82307cf6ddb6
Jeremy Howard <github@jhoward.fastmail.fm>
2025-04-16 12:58:19 +1000

    cleanup

--- oauth_example/minimal.py
+++ oauth_example/minimal.py
@@ -2,20 +2,16 @@ from fasthtml.common import *
 from fasthtml.oauth import GitHubAppClient
 import json
 
-# Auth client
-client = GitHubAppClient(os.getenv("AUTH_CLIENT_ID"),
-                         os.getenv("AUTH_CLIENT_SECRET"))
+client = GitHubAppClient(os.getenv("AUTH_CLIENT_ID"), os.getenv("AUTH_CLIENT_SECRET"))
 redirect_uri = "http://localhost:5001/redirect"
+app,rt = fast_app()
 
-app = FastHTML()
+@rt
+def index():
+    return P("Login link: ", A("click here", href=client.login_link(redirect_uri)))
 
-@app.get('/')
-def home():
-    link = client.login_link(redirect_uri)
-    return P("Login link: ", A("click here", href=link))
-
-@app.get('/redirect')
-def auth_redirect(request, code:str):
+@rt
+def redirect(request, code:str):
     info = client.retr_info(code, redirect_uri)
     return H1("Profile info"), P(json.dumps(info))
 
