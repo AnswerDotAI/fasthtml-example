dd0a669471b8d198ffcaa857d25a6168709384b3
Jeremy Howard <github@jhoward.fastmail.fm>
2025-05-30 12:58:22 +1000

    style

--- htmx/formdemo.py
+++ htmx/formdemo.py
@@ -1,17 +1,18 @@
 from fasthtml.common import *
 app,rt = fast_app(live=True)
 
-@rt('/')
-def get():
+@rt
+def submit(d:dict): return d
+
+@rt
+def index():
     return Titled('HTMX Form Demo', Grid(
-        Form(hx_post="/submit", hx_target="#result", hx_trigger="input delay:200ms")(
+        Form(hx_post=submit, hx_target="#result", hx_trigger="input delay:200ms")(
             Select(Option("One"), Option("Two"), id="select"),
             Input(value='j', type="text", id="name", placeholder="Name"),
             Input(value='h', type="text", id="email", placeholder="Email")),
+        Button('Click me', hx_post=submit, hx_vals=dict(a=1,b=2), hx_target="#result"),
         Div(id="result")
     ))
-
-@rt('/submit')
-def post(d:dict): return d
 
 serve()
