01bc5dba0d00d72ca78ceb46eb65583211fc81a5
ncoop57 <nathanacooper@proton.me>
2025-08-11 15:30:02 -0400

    add proper resizing and clean up code
    Co-authored-by: Jeremy Howard <jph00@users.noreply.github.com>

--- xtermjs/static/terminal.js
+++ xtermjs/static/terminal.js
@@ -1,14 +1,22 @@
-var term = new Terminal();
+import { FitAddon } from 'https://cdn.jsdelivr.net/npm/xterm-addon-fit@0.8.0/+esm';
+
+const term = new Terminal();
+const fitAddon = new FitAddon();
+term.loadAddon(fitAddon);
 term.open(document.getElementById('terminal'));
+// fitAddon.fit();
 
-const socket = new WebSocket(`${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}/ws`);
-socket.onopen  = () => term.focus();
+const socket = new WebSocket(`/ws`);
+
+function resizeTerm() {
+  fitAddon.fit();
+  socket.send(JSON.stringify({cols:term.cols, rows:term.rows, HEADERS: {}}));
+}
+window.addEventListener('resize', () => resizeTerm());
+
+socket.onopen = () => {
+  resizeTerm();
+  term.focus();
+};
 socket.onmessage = ev => term.write(ev.data);
-
-term.onKey(e =>{
-  const ev = e.domEvent;
-  const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
-  if (printable) {
-    socket.send(JSON.stringify({msg: e.key, HEADERS: {}}));
-  }
-});
+term.onData(data => {socket.send(JSON.stringify({msg: data, HEADERS: {}}));});
