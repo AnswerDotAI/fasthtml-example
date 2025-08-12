ca65d7f774fb8b32b95b7d0ec2f037d9417ae13d
ncoop57 <nathanacooper@proton.me>
2025-08-12 11:43:23 -0400

    add logout all sessions example
    Co-authored-by: RensDimmendaal <RensDimmendaal@users.noreply.github.com>

---
+++ logout_all/main.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from datetime import datetime, UTC
+from fasthtml.common import *
+from fasthtml.oauth import GoogleAppClient, OAuth
+
+import os
+
+now = lambda: int(datetime.now(UTC).timestamp())
+
+@dataclass
+class User: id: str; logout_time: int
+db = database('/tmp/data.db')
+db.users = db.create(User, transform=True)
+
+logout_time = int(datetime.now(UTC).timestamp())
+
+class Auth(OAuth):
+    def get_auth(self, info, ident, session, state):
+        if not info.sub: RedirectResponse('/login', status_code=303)
+        db.users.upsert(User(id=info.sub, logout_time=now()))
+        session['login_time'] = now()
+        return RedirectResponse('/', status_code=303)
+    
+    def check_invalid(self, req, session, auth):
+        if session.get('login_time', 0) < db.users[auth].logout_time:
+            session.pop('auth', None)
+            return RedirectResponse('/login', status_code=419)
+        return False
+
+app = FastHTML()
+cli = GoogleAppClient(os.environ['GOOGLE_CLIENT_ID'], os.environ['GOOGLE_CLIENT_SECRET'])
+oauth = Auth(app, cli)
+
+@app.get('/logout')
+def logout(session):
+    if auth := session.pop('auth', None): db.users.upsert(id=auth, logout_time=now())
+    return oauth.redir_login(session)
+
+@app.get('/')
+def home(auth): return P('Logged in!'), A('Log out', href='/logout')
+
+@app.get('/login')
+def login(req): return Div(P("Not logged in"), A('Log in', href=oauth.login_link(req)))
+
+serve()
